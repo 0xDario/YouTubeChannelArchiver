b9dab8caae2796e513a0acaf1c485eb70f49bebb
Dario Turchi <61662791+0xDario@users.noreply.github.com>
2023-06-13 16:38:35 -0400

    create variable for ease of use

--- yt-dlp-script.bat
+++ yt-dlp-script.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 REM Downloads Video, converts into MKV and embeds subtitles
 REM https://askubuntu.com/questions/1022855/download-everything-from-a-youtube-video-using-youtube-dl/1022993#1022993
-IF NOT EXIST "./archive/videos/LearnItalianWithLucrezia/" MKDIR "./archive/videos/LearnItalianWithLucrezia/" 
-IF NOT EXIST "./archive/videos/LearnItalianWithLucrezia/LearnItalianWithLucrezia.ytdlarchive" ECHO. > "./archive/videos/LearnItalianWithLucrezia/LearnItalianWithLucrezia.ytdlarchive" 
+set path="LearnItalianWithLucrezia"
+set youtubeURL="https://www.youtube.com/@lucreziaoddone/videos"
+IF NOT EXIST "./archive/videos/%folderPath%/" MKDIR "./archive/videos/%folderPath%/" 
+IF NOT EXIST "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" ECHO. > "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" 
 yt-dlp ^
 --retries "3" ^
 --no-overwrites ^
@@ -15,14 +17,14 @@ yt-dlp ^
 --write-annotations ^
 --add-metadata ^
 --embed-subs ^
---download-archive "./archive/videos/LearnItalianWithLucrezia/LearnItalianWithLucrezia.ytdlarchive" ^
+--download-archive "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" ^
 --format "bestvideo+bestaudio/best" ^
 --merge-output-format "mkv" ^
 --ffmpeg-location "C:\Users\Lupin\Desktop\ffmpeg\bin\ffmpeg.exe" ^
---output "./archive/videos/LearnItalianWithLucrezia/%%(upload_date)s_%%(id)s/LearnItalianWithLucrezia_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
-"https://www.youtube.com/@lucreziaoddone/videos"
+--output "./archive/videos/%folderPath%/%%(upload_date)s_%%(id)s/%folderPath%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
+"%youtubeURL%"
 
-IF NOT EXIST "./archive/videos/LearnItalianWithLucrezia/LearnItalianWithLucrezia.ytdlarchive" ECHO. > "./archive/videos/LearnItalianWithLucrezia/LearnItalianWithLucrezia_subtitles.ytdlarchive" 
+IF NOT EXIST "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" ECHO. > "./archive/videos/%folderPath%/%folderPath%_subtitles.ytdlarchive" 
 REM Downloads additional externally available SUBTITLES to the folder of downloaded video
 yt-dlp ^
 --skip-download ^
@@ -30,8 +32,8 @@ yt-dlp ^
 --call-home ^
 --sub-langs "en" ^
 --convert-subs "srt" ^
---output "./archive/videos/LearnItalianWithLucrezia/%%(upload_date)s_%%(id)s/LearnItalianWithLucrezia_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
-"https://www.youtube.com/@lucreziaoddone/videos"
+--output "./archive/videos/%folderPath%/%%(upload_date)s_%%(id)s/%folderPath%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
+"%youtubeURL%"
 
 
 REM Downloads additional and missing THUMBNAILS
@@ -40,6 +42,6 @@ yt-dlp ^
 --write-all-thumbnails ^
 --ignore-config ^
 --id ^
---output "./archive/videos/LearnItalianWithLucrezia/%%(upload_date)s_%%(id)s/LearnItalianWithLucrezia_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
-"https://www.youtube.com/@lucreziaoddone/videos"
+--output "./archive/videos/%folderPath%/%%(upload_date)s_%%(id)s/%folderPath%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
+"%youtubeURL%"
 PAUSE
