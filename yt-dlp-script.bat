e068d253fa3e25157d8c5aa832322f61fc2d60ce
Dario Turchi <61662791+0xDario@users.noreply.github.com>
2023-06-13 16:48:42 -0400

    fix path variable

--- yt-dlp-script.bat
+++ yt-dlp-script.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 REM Downloads Video, converts into MKV and embeds subtitles
 REM https://askubuntu.com/questions/1022855/download-everything-from-a-youtube-video-using-youtube-dl/1022993#1022993
-set path="LearnItalianWithLucrezia"
+set folder="LearnItalianWithLucrezia"
 set youtubeURL="https://www.youtube.com/@lucreziaoddone/videos"
-IF NOT EXIST "./archive/videos/%folderPath%/" MKDIR "./archive/videos/%folderPath%/" 
-IF NOT EXIST "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" ECHO. > "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" 
+IF NOT EXIST "./archive/videos/%folder%/" MKDIR "./archive/videos/%folder%/" 
+IF NOT EXIST "./archive/videos/%folder%/%folder%.ytdlarchive" ECHO. > "./archive/videos/%folder%/%folder%.ytdlarchive" 
 yt-dlp ^
 --retries "3" ^
 --no-overwrites ^
@@ -17,14 +17,14 @@ yt-dlp ^
 --write-annotations ^
 --add-metadata ^
 --embed-subs ^
---download-archive "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" ^
+--download-archive "./archive/videos/%folder%/%folder%.ytdlarchive" ^
 --format "bestvideo+bestaudio/best" ^
 --merge-output-format "mkv" ^
---ffmpeg-location "C:\Users\Lupin\Desktop\ffmpeg\bin\ffmpeg.exe" ^
---output "./archive/videos/%folderPath%/%%(upload_date)s_%%(id)s/%folderPath%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
+--ffmpeg-location ".\ffmpeg\bin\ffmpeg.exe" ^
+--output "./archive/videos/%folder%/%%(upload_date)s_%%(id)s/%folder%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
 "%youtubeURL%"
 
-IF NOT EXIST "./archive/videos/%folderPath%/%folderPath%.ytdlarchive" ECHO. > "./archive/videos/%folderPath%/%folderPath%_subtitles.ytdlarchive" 
+IF NOT EXIST "./archive/videos/%folder%/%folder%.ytdlarchive" ECHO. > "./archive/videos/%folder%/%folder%_subtitles.ytdlarchive" 
 REM Downloads additional externally available SUBTITLES to the folder of downloaded video
 yt-dlp ^
 --skip-download ^
@@ -32,7 +32,7 @@ yt-dlp ^
 --call-home ^
 --sub-langs "en" ^
 --convert-subs "srt" ^
---output "./archive/videos/%folderPath%/%%(upload_date)s_%%(id)s/%folderPath%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
+--output "./archive/videos/%folder%/%%(upload_date)s_%%(id)s/%folder%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
 "%youtubeURL%"
 
 
@@ -42,6 +42,6 @@ yt-dlp ^
 --write-all-thumbnails ^
 --ignore-config ^
 --id ^
---output "./archive/videos/%folderPath%/%%(upload_date)s_%%(id)s/%folderPath%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
+--output "./archive/videos/%folder%/%%(upload_date)s_%%(id)s/%folder%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
 "%youtubeURL%"
 PAUSE
