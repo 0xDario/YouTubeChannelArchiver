34c89a20e57f473af4748f5ac2ce2533e23a9fb0
Dario Turchi <darioturchi@outlook.com>
2024-11-08 01:18:40 -0500

    Enhance README and script for improved user input and clarity

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ Download an entire youtube channel with annontaions, subtitles and metadata
 ### 2.) Install FFMPEG
 - unzip FFMPEG folder into root of project directory
 - https://www.gyan.dev/ffmpeg/builds/
+- ```winget install ffmpeg```
 
 ### 3.) Install yt-dlp
 - place .exe in root of project directory
 - https://github.com/yt-dlp/yt-dlp/releases
 
 ### 4.) Change the two variables inside the .bat file
-- **path** is for name of directory you want to download (channel name is good choice)
-- **youtubeURL** is for full youtube.com channell url
+- **path** is for name of directory you want to download (channel name is good choice, name of video)
+- **youtubeURL** is for full youtube.com channel or video URL
--- yt-dlp-script.bat
+++ yt-dlp-script.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 REM Downloads Video, converts into MKV and embeds subtitles
 REM https://askubuntu.com/questions/1022855/download-everything-from-a-youtube-video-using-youtube-dl/1022993#1022993
-set folder="LearnItalianWithLucrezia"
-set youtubeURL="https://www.youtube.com/@lucreziaoddone/videos"
+set /p folder="Enter the download directory: "
+set /p youtubeURL="Enter the URL (video, channel, or playlist): "
 IF NOT EXIST "./archive/videos/%folder%/" MKDIR "./archive/videos/%folder%/" 
 IF NOT EXIST "./archive/videos/%folder%/%folder%.ytdlarchive" ECHO. > "./archive/videos/%folder%/%folder%.ytdlarchive" 
 yt-dlp ^
@@ -20,28 +20,6 @@ yt-dlp ^
 --download-archive "./archive/videos/%folder%/%folder%.ytdlarchive" ^
 --format "bestvideo+bestaudio/best" ^
 --merge-output-format "mkv" ^
---ffmpeg-location ".\ffmpeg\bin\ffmpeg.exe" ^
+--ffmpeg-location "C:\ProgramData\chocolatey\lib\ffmpeg\tools\ffmpeg\bin" ^
 --output "./archive/videos/%folder%/%%(upload_date)s_%%(id)s/%folder%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
 "%youtubeURL%"
-
-IF NOT EXIST "./archive/videos/%folder%/%folder%.ytdlarchive" ECHO. > "./archive/videos/%folder%/%folder%_subtitles.ytdlarchive" 
-REM Downloads additional externally available SUBTITLES to the folder of downloaded video
-yt-dlp ^
---skip-download ^
---retries "3" ^
---call-home ^
---sub-langs "en" ^
---convert-subs "srt" ^
---output "./archive/videos/%folder%/%%(upload_date)s_%%(id)s/%folder%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
-"%youtubeURL%"
-
-
-REM Downloads additional and missing THUMBNAILS
-yt-dlp ^
---skip-download ^
---write-all-thumbnails ^
---ignore-config ^
---id ^
---output "./archive/videos/%folder%/%%(upload_date)s_%%(id)s/%folder%_%%(upload_date)s_%%(id)s_%%(title)s.%%(ext)s" ^
-"%youtubeURL%"
-PAUSE
